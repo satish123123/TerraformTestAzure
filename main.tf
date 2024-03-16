6f6469ce23b94f1416dab0b9b3675849e7ae2520
satishfc <you@example.com>
2024-03-16 17:14:57 +0530

    devopsnew

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "azurerm_network_interface" "vmnic" {
 }
 # Create our Virtual Machine - satishdevops-VM01
 resource "azurerm_virtual_machine" "satishdevopsvm01" {
-  name                  = "satishdevopsvm01"
+  name                  = "devopsvm01"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.vmnic.id]
