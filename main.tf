98a684a86e5ca6127c4b1499caa74c9c5969aee6
satishfc <you@example.com>
2024-03-16 17:06:28 +0530

    devops

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "azurerm_storage_account" "satishdevopssa" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   tags = {
-    environment = "satishdevopsenv122new"
+    environment = "satishdevopsenv122newdevops"
   }
 }
 # Create our vNIC for our VM and assign it to our Virtual Machines Subnet
